164d4f651b29dba0c6c01fc82ff42f25d0b5db12
Sal Espinosa <sespinos@gmail.com>
2020-05-06 14:58:32 -0600

    Initial Commit

---
+++ practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  practice
+//
+//  Created by Salvador E Espinosa on 5/6/20.
+//  Copyright © 2020 Salvador E Espinosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
